377a91eb118370a9e2c7cb3df9ecb552d4dc0e98
Scott Cottam <scott@scottcottam.co.uk>
2018-02-08 22:13:24 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<form role="form" action="post.php" method="post">
+  <textarea name="message" rows="8" cols="80"></textarea>
+  <button type="submit">Post</button>
+</form>
---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+
+$message = $_POST['message'];
+
+$arr = array('content' => $message);
+
+echo json_encode($arr);
+
+$url = 'https://discordapp.com/api/webhooks/274628909138575360/Im39Wt9hslEtdGwu_lMt3tUI39DeCmNhvlrnXeGM3v_nAnfUUSKzrMd32nt-JBmhqP5d'
+
+
+
+?>
